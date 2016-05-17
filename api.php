71297d8c7240246e49de8cf1aea61f610c7c0a19
WilliamGDavis <will@willdavis.net>
2016-05-17 15:14:31 -0400

    Lots of additions regarding Punching in and Punching Out

--- api.php
+++ api.php
@@ -55,9 +55,67 @@ function PunchIn($employeeId, $type, $open_status) {
     }
 }
 
+function PunchOut($employeeId, $type, $open_status) {
+    $params = [
+        "employeeId" => $employeeId,
+        "type" => $type,
+        "open_status" => $open_status
+    ];
+
+    try {
+        return Employee::PunchOut($params);
+    } catch (Exception $ex) {
+        return $ex->getMessage();
+    }
+}
+
+function CheckLoginStatus($employeeId) {
+    try {
+        return Employee::CheckLoginStatus($employeeId);
+    } catch (Exception $ex) {
+        return $ex->getMessage();
+    }
+}
+
 function test_connection() {
     $ConnectionStatus = new DBConnect();
     return $ConnectionStatus->CheckConnection();
+}
+
+function CheckCurrentJob($employeeId) {
+    try {
+        return Employee::CheckCurrentJob($employeeId);
+    } catch (Exception $ex) {
+        return $ex->getMessage();
+    }
+}
+
+function ChangeJob($employeeId, $jobId, $newJobId) {
+    try {
+        return Employee::ChangeJob($employeeId, $jobId, $newJobId);
+    } catch (Exception $ex) {
+        return $ex->getMessage();
+    }
+}
+
+function JobLookup($jobDescription){
+    try {
+        return Employee::JobLookup($jobDescription);
+    } catch (Exception $ex) {
+        return $ex->getMessage();
+    }
+}
+
+function JobPunch($employeeId, $newJobId){
+    try {
+        return Employee::JobPunch($employeeId, $newJobId);
+    } catch (Exception $ex) {
+        return $ex->getMessage();
+    }
+}
+
+function get_settings() {
+    return Employee::GetSettings();
 }
 
 function get_current_job_by_employee_id($id) {
@@ -76,7 +134,14 @@ $possible_url = array("get_employee",
     "test_connection",
     "get_current_job_number",
     "add_user",
-    "PunchIn"
+    "PunchIn",
+    "PunchOut",
+    "CheckLoginStatus",
+    "RetrieveSettings",
+    "CheckCurrentJob",
+    "ChangeJob",
+    "JobLookup",
+    "JobPunch"
 );
 $value = "An error has occured";
 
@@ -107,8 +172,7 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url)) {
         case 'add_user':
             break;
         case 'get_all_employees':
-//            $value = get_all_employees();
-            $value = "got here";
+            $value = get_all_employees();
             break;
         case 'Pin_Login':
             if (isset($_POST["pin"])) {
@@ -118,16 +182,68 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url)) {
             }
             break;
         case 'PunchIn':
-//            if (isset($_POST["employeeId"])) {
-//                $value = PunchIn($_POST['employeeId'], $_POST['type'], $_POST['open_status']);
-//            } else {
-//                $value = null;
-//            }
-            $value = "got here";
+            if (isset($_POST["employeeId"])) {
+                $value = PunchIn($_POST['employeeId'], $_POST['type'], $_POST['open_status']);
+            } else {
+                $value = null;
+            }
+            break;
+        case 'PunchOut':
+            if (isset($_POST["employeeId"])) {
+                $value = PunchOut($_POST['employeeId'], $_POST['type'], $_POST['open_status']);
+            } else {
+                $value = null;
+            }
+            break;
+        case 'CheckLoginStatus':
+            if (isset($_POST['employeeId'])) {
+                $value = CheckLoginStatus($_POST['employeeId']);
+            } else {
+                $value = null;
+            }
+            break;
+        case 'CheckCurrentJob':
+            if (isset($_POST['employeeId'])) {
+                $value = CheckCurrentJob($_POST['employeeId']);
+            } else {
+                $value = null;
+            }
+            break;
+        case 'ChangeJob':
+            if (isset($_POST['employeeId']) && isset($_POST['jobId']) && isset($_POST['newJobId'])) {
+                $value = ChangeJob($_POST['employeeId'], $_POST['jobId'], $_POST['newJobId']);
+            } else {
+                $value = null;
+            }
+            break;
+        case 'JobLookup':
+            if (isset($_POST['jobDescription'])) {
+                $value = JobLookup($_POST['jobDescription']);
+            } else {
+                $value = null;
+            }
+            break;
+            case 'JobPunch':
+            if (isset($_POST['employeeId']) && isset($_POST['newJobId'])) {
+                $value = JobPunch($_POST['employeeId'], $_POST['newJobId']);
+            } else {
+                $value = null;
+            }
+            break;
+        case 'RetrieveSettings':
+            $value = get_settings();
             break;
         default:
             break;
     }
+} elseif ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    $value = parse_str(file_get_contents("php://input"));
+//    switch ($putArray['action']){
+//        case 'PunchOut':
+//            $value = "Got here";
+//        default:
+//            break;
+//    }
 }
 
 //return JSON array
--- classes/Employee.php
+++ classes/Employee.php
@@ -26,7 +26,35 @@ class Employee {
     }
 
     public static function PunchIn($params) {
-        return self::PunchIntoClock($params);
+        return self::PunchInClock($params);
+    }
+
+    public static function PunchOut($params) {
+        return self::PunchOutClock($params);
+    }
+
+    public static function CheckLoginStatus($employeeId) {
+        return self::CheckForLoginStatus($employeeId);
+    }
+
+    public static function CheckCurrentJob($employeeId) {
+        return self::CheckForCurrentJob($employeeId);
+    }
+
+    public static function GetSettings() {
+        return self::ReturnAllSettings();
+    }
+
+    public static function ChangeJob($employeeId, $jobId, $newJobId) {
+        return self::ChangeJobInDb($employeeId, $jobId, $newJobId);
+    }
+
+    public static function JobLookup($jobDescription) {
+        return self::JobLookupFromDb($jobDescription);
+    }
+
+    public static function JobPunch($employeeId, $newJobId) {
+        return self::JobPunchToDb($employeeId, $newJobId);
     }
 
     /*
@@ -58,6 +86,26 @@ class Employee {
         }
 
         return $employee_array;
+    }
+
+    private static function ReturnAllSettings() {
+        $db = new DBConnect();
+        $db = $db->DBObject;
+
+        $query = "SELECT id, name, value "
+                . "FROM settings";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+
+        $settings_array = [];
+        while ($row = $stmt->fetchObject()) {
+            array_push($settings_array, array(
+                'id' => $row->id,
+                'name' => $row->name,
+                'value' => $row->value
+            ));
+        }
+        return $settings_array;
     }
 
     private static function LoginViaPIN($pin) {
@@ -100,23 +148,275 @@ class Employee {
         return $employee;
     }
 
-    private static function PunchIntoClock($params) {
+    //TODO: Create a function to check for an "open" punch to marry a PunchIn to a PunchOut
+    /*
+     * Function: Add a PunchIn stamp to the database
+     * It also adds a punch to the punches_open table in order to keep track of any open punches
+     *
+     */
+    private static function PunchInClock($params) {
+        //Temp
+        if ($params["type"] == "in") {
+            $params["type"] = 1;
+            $params["open_status"] = 1;
+        }
         try {
             $db = new DBConnect();
             $db = $db->DBObject;
-            $query = "INSERT INTO punches (id, id_users, id_jobs, timestamp, type, open_status)"
-                    . "VALUES (:id, :id_users, :id_jobs, :timestamp, :type, :open_status)";
+            $query = "INSERT INTO punches (id, id_users, id_jobs, id_parentPunch, type, open_status)"
+                    . "VALUES (:id, :id_users, :id_jobs, :id_parentPunch, :type, :open_status)";
             $stmt = $db->prepare($query);
             $stmt->execute(array(
                 ":id" => null,
                 ":id_users" => $params["employeeId"],
                 ":id_jobs" => 0,
-                ":timestamp" => null,
-                ":type" => 1, //TODO: Call a function to convert 'in' to 1
-                ":open_status" => $params["open_status"]
+                ":id_parentPunch" => 0,
+                ":type" => $params["type"], //TODO: Call a function to convert 'in' to 1
+                ":open_status" => $params["open_status"] //TODO: Call a function to convert $params["open_status"]
+            ));
+            $last_insert_id = $db->lastInsertId();
+            $query = "INSERT INTO punches_open (id, id_punches, id_users)"
+                    . "VALUES (:id, :id_punches, :id_users)";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":id" => null,
+                ":id_punches" => $last_insert_id,
+                ":id_users" => $params['employeeId']
             ));
         } catch (Exception $ex) {
             return $ex->getMessage();
+        }
+    }
+
+    private static function PunchOutClock($params) {
+        try {
+            $db = new DBConnect();
+            $db = $db->DBObject;
+            $punch = [];
+
+            //TODO: Use a transaction
+            //Find the Open Punch for the user
+            $query = "SELECT id, id_punches "
+                    . "FROM punches_open "
+                    . "WHERE id_users = :id_users "
+                    . "LIMIT 1";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ':id_users' => $params['employeeId']
+            ));
+
+            while ($row = $stmt->fetchObject()) {
+                $punch['id'] = $row->id;
+                $punch['id_punches'] = $row->id_punches;
+            }
+
+            //Delete the Open Punch in the table
+            $query = "DELETE FROM punches_open "
+                    . "WHERE id = :id "
+                    . "LIMIT 1";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ':id' => $punch['id']
+            ));
+
+            //Insert the PunchOut
+            $query = "INSERT INTO punches (id, id_users, id_jobs, id_parentPunch, type, open_status)"
+                    . "VALUES (:id, :id_users, :id_jobs, :id_parentPunch, :type, :open_status)";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":id" => null,
+                ":id_users" => $params["employeeId"],
+                ":id_jobs" => 0,
+                ':id_parentPunch' => $punch['id_punches'],
+                ":type" => $params["type"], //TODO: Call a function to convert 'in' to 1
+                ":open_status" => $params["open_status"] //TODO: Call a function to convert $params["open_status"]
+            ));
+
+            //UPDATE the original "parent" punch
+            $query = "UPDATE punches "
+                    . "SET open_status = :open_status "
+                    . "WHERE id = :id_punches "
+                    . "LIMIT 1";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":open_status" => $params['open_status'],
+                ":id_punches" => $punch['id_punches']
+            ));
+        } catch (Exception $ex) {
+            return $ex->getMessage();
+        }
+    }
+
+    private static function ChangeJobInDb($employeeId, $jobId, $newJobId) {
+        try {
+            $db = new DBConnect();
+            $db = $db->DBObject;
+            $jobPunch = [];
+            //TODO: Use a transaction
+            //Find the Parent Punch for Open Job Punch for the user
+            $query = "SELECT id, id_punches_jobs "
+                    . "FROM punches_jobs_open "
+                    . "WHERE id_users = :id_users "
+                    . "LIMIT 1";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ':id_users' => $employeeId
+            ));
+
+            while ($row = $stmt->fetchObject()) {
+                $jobPunch['id'] = $row->id;
+                $jobPunch['id_punches_jobs'] = $row->id_punches_jobs;
+            }
+
+            //Delete the currently open punch
+            $query = "DELETE FROM punches_jobs_open "
+                    . "WHERE id = :id "
+                    . "LIMIT 1";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ':id' => $jobPunch['id']
+            ));
+
+            //"Close" out the original parent job punch
+            $query = "UPDATE punches_jobs "
+                    . "SET open_status = :open_status "
+                    . "WHERE id = :id_punches_jobs "
+                    . "LIMIT 1";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":open_status" => 0,
+                ":id_punches_jobs" => $jobPunch['id_punches_jobs']
+            ));
+
+            //Add a PunchOut to the Database
+            $query = "INSERT INTO punches_jobs (id, id_jobs, id_parent_punch_jobs, id_users, type, open_status)"
+                    . "VALUES (:id, :id_jobs, :id_parent_punch_jobs, :id_users, :type, :open_status)";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":id" => null,
+                ":id_jobs" => $jobId,
+                ":id_parent_punch_jobs" => $jobPunch['id_punches_jobs'],
+                ":id_users" => $employeeId,
+                ":type" => 0,
+                ":open_status" => 0
+            ));
+
+            //Add the new punch to the Database
+            $query = "INSERT INTO punches_jobs (id, id_jobs, id_parent_punch_jobs, id_users, type, open_status)"
+                    . "VALUES (:id, :id_jobs, :id_parent_punch_jobs, :id_users, :type, :open_status)";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":id" => null,
+                ":id_jobs" => $newJobId,
+                ":id_parent_punch_jobs" => 0,
+                ":id_users" => $employeeId,
+                ":type" => 1,
+                ":open_status" => 1
+            ));
+            $last_insert_id = $db->lastInsertId();
+
+            //Create an "Open" punch for the user
+            $query = "INSERT INTO punches_jobs_open (id, id_punches_jobs, id_users)"
+                    . "VALUES (:id, :id_punches_jobs, :id_users)";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":id" => null,
+                ":id_punches_jobs" => $last_insert_id,
+                ":id_users" => $employeeId
+            ));
+        } catch (Exception $ex) {
+            return $ex->getMessage();
+        }
+    }
+
+    private static function CheckForLoginStatus($employeeId) {
+        $db = new DBConnect();
+        $db = $db->DBObject;
+
+        //Counting Query
+        $query = "SELECT COUNT(*) "
+                . "FROM `punches_open` "
+                . "WHERE id_users = :id_users";
+        $stmt = $db->prepare($query);
+        $stmt->execute(array(
+            ":id_users" => $employeeId
+        ));
+
+        $count = $stmt->fetchColumn();
+        if (1 == $count) {
+            return true;
+        } elseif (0 == $count) {
+            return false;
+        } else {
+            return null;
+        }
+    }
+
+    private static function CheckForCurrentJob($employeeId) {
+        $db = new DBConnect();
+        $db = $db->DBObject;
+        $currentJobId = null;
+        $currentJobArray = [];
+
+        //Counting Query
+        $query = "SELECT COUNT(*) "
+                . "FROM `punches_jobs_open` "
+                . "WHERE id_users = :id_users";
+        $stmt = $db->prepare($query);
+        $stmt->execute(array(
+            ":id_users" => $employeeId
+        ));
+
+        $count = $stmt->fetchColumn();
+
+        if (1 <= $count) {
+            $query = "SELECT id_punches_jobs  "
+                    . "FROM `punches_jobs_open` "
+                    . "WHERE id_users = :id_users "
+                    . "LIMIT 1";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":id_users" => $employeeId
+            ));
+
+            while ($row = $stmt->fetchObject()) {
+                $currentJobId = $row->id_punches_jobs;
+            }
+
+            //Find the job id
+            $query = "SELECT id_jobs  "
+                    . "FROM `punches_jobs` "
+                    . "WHERE id = :id_currentJob "
+                    . "LIMIT 1";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":id_currentJob" => $currentJobId
+            ));
+
+            while ($row = $stmt->fetchObject()) {
+                $jobId = $row->id_jobs;
+            }
+
+            //Find the job data
+            $query = "SELECT id, description, code, active  "
+                    . "FROM `jobs` "
+                    . "WHERE id = :id_jobs "
+                    . "LIMIT 1";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":id_jobs" => $jobId
+            ));
+
+            while ($row = $stmt->fetchObject()) {
+                $currentJobArray["id"] = $row->id;
+                $currentJobArray["description"] = $row->description;
+                $currentJobArray["code"] = $row->code;
+                $currentJobArray["active"] = $row->active;
+            }
+
+            return $currentJobArray;
+        } else {
+            return null;
         }
     }
 
@@ -173,6 +473,61 @@ class Employee {
             $punch = $row->description;
         }
         return $punch;
+    }
+
+    private static function JobLookupFromDb($jobDescription) {
+        $db = new DBConnect();
+        $db = $db->DBObject;
+        $jobNumber = null;
+
+        //Actual Query
+        $query = "SELECT id "
+                . "FROM `jobs` "
+                . "WHERE description = :jobDescription "
+                . "LIMIT 1";
+        $stmt = $db->prepare($query);
+        $stmt->execute(array(
+            ":jobDescription" => $jobDescription
+        ));
+
+        while ($row = $stmt->fetchObject()) {
+            $jobNumber = $row->id;
+        }
+
+        return $jobNumber;
+    }
+
+    private static function JobPunchToDb($employeeId, $newJobId) {
+        try {
+            $db = new DBConnect();
+            $db = $db->DBObject;
+            
+            //Add the new punch to the Database
+            $query = "INSERT INTO punches_jobs (id, id_jobs, id_parent_punch_jobs, id_users, type, open_status)"
+                    . "VALUES (:id, :id_jobs, :id_parent_punch_jobs, :id_users, :type, :open_status)";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":id" => null,
+                ":id_jobs" => $newJobId,
+                ":id_parent_punch_jobs" => 0,
+                ":id_users" => $employeeId,
+                ":type" => 1,
+                ":open_status" => 1
+            ));
+            $last_insert_id = $db->lastInsertId();
+
+            //Create an "Open" punch for the user
+            $query = "INSERT INTO punches_jobs_open (id, id_punches_jobs, id_users)"
+                    . "VALUES (:id, :id_punches_jobs, :id_users)";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(
+                ":id" => null,
+                ":id_punches_jobs" => $last_insert_id,
+                ":id_users" => $employeeId
+            ));
+        } catch (Exception $ex) {
+            return $ex->getMessage();
+        }
     }
 
 }
